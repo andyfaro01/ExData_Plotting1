167ad07f98e4931f7a63ac82f3c556b1738eaf44
anyfaro01 <andyfaro01@yahoo.com>
2017-07-27 22:32:46 +0100

    copying plot to file plot1.png

--- plot1.R
+++ plot1.R
@@ -33,3 +33,6 @@ str(household_power_consumption)
 ##Generating plot
 hist(household_power_consumption$Global_active_power, main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+## Saving to file
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
